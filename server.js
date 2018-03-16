2a896aa18e8dc6dc394d69c382251592b7c78d37
Jose Diaz-Nunez <dia15012@byui.edu>
2018-03-16 02:23:01 -0600

    Added test files

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+const path    = require('path');
+const PORT    = process.env.PORT || 5000;
 const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const app     = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-   .set('views', path.join(__dirname, 'views'))
-   .set('view engine', 'ejs')
-   .get('/', (req, res) => console.log('Hello world!'))
-   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/result');
+});
+
+app.listen(PORT, function() { 
+	console.log(`Listening on port ${ PORT }`);
+});
