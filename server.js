f5cba13294f23e4f82f29c81d01050b3ef25861d
Jose Diaz-Nunez <dia15012@byui.edu>
2018-03-17 22:28:31 -0600

    Added week 10 progress

--- server.js
+++ server.js
@@ -1,17 +1,291 @@
-const path    = require('path');
-const PORT    = process.env.PORT || 5000;
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const express = require('express');
-const app     = express();
+const app = express();
+const bodyParser = require('body-parser');
+var pg = require("pg");
+const connectionString = process.env.DATABASE_URL || "postgres://test_user:password@localhost:5432/weekly_planner";
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/public')));
+app.use("/public/stylesheets", express.static(__dirname + "/public/stylesheets"));
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.render('pages/result');
+	getDaysAndTasks(req, res);
+});
+
+// app.get('/:id', function(req, res) {
+// 	console.log("deleting task...");
+// 	getDaysAndTasks(req, res);
+// });
+
+// app.delete('/?', function(req, res) {
+// 	console.log("deleting task...");
+// 	getDaysAndTasks(req, res);
+// });
+
+app.post('/', function(req, res) {
+	if (req.body.available_time != null) {
+		updateAvailableTime(req, res);
+	} else if (req.body.task_id != null) {
+		deleteTask(req, res);
+	} else {
+		createNewTask(req, res);
+	}
 });
 
 app.listen(PORT, function() { 
 	console.log(`Listening on port ${ PORT }`);
 });
+
+function getDaysAndTasks(request, response) {
+	// Use a helper function to query DB and provide callback after processing
+	getDaysAndTasksFromDb(function(error, result) {
+		// Callback function that will be called when the DB done
+		if (error || result == null) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			// response.status(200).json(result);
+			var days = {};
+			days[1]  = {};
+			days[2]  = {};
+			days[3]  = {};
+			days[4]  = {};
+			days[5]  = {};
+			days[6]  = {};
+			days[7]  = {};
+
+			for (const index in result) {
+				var dayId = result[index].day_id;
+				days[dayId]["name"] = result[index].name;
+				days[dayId]["total_available_time_day"] = result[index].total_available_time_day;
+				days[dayId]["total_work_time_day"] = result[index].total_work_time_day;
+				// days[dayId]["day_id"] = dayId;
+			}
+
+			response.render('pages/planner', {
+				daysAndTasks: result,
+				days: days
+			});	
+		}
+	});
+}
+
+function getDaysAndTasksFromDb(callback) {
+	console.log("Getting days from DB");
+
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		var sql =  "SELECT *                      \
+		           	FROM task_day as td           \
+					INNER JOIN task as t          \
+						ON td.task_id = t.task_id \
+					RIGHT JOIN day as d           \
+						ON td.day_id = d.day_id;";
+
+		var query = client.query(sql, function(err, result) {
+			// Done getting data from DB; disconnect the client
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+
+			// Giving results to callback
+			callback(null, result.rows);
+		});
+	});
+}
+
+function createNewTask(req, res) {
+	insertNewTaskIntoDb(function (error, result) {
+		console.log("task id i got back yo: " + result.last_task_id);
+		console.log("day id i got back yo: " + result.last_day_id);
+		console.log("work time i got back yo: " + result.work_time);
+
+		console.log("Inserting new task_day value into DB");
+
+		var client = new pg.Client(connectionString);
+
+		client.connect(function(err) {
+			if (err) {
+				console.log("Error connecting to DB: ")
+				console.log(err);
+			}
+
+			var sql = "INSERT INTO task_day" 		+
+						"("							+
+						  "task_id"					+
+						", day_id"					+
+						") "						+
+						"VALUES"					+
+						"("							+ 
+						      result.last_task_id 	+ 
+						"," + result.last_day_id	+ 
+						")";
+			
+			var sqlUpdate = "UPDATE day SET total_work_time_day = total_work_time_day + " + 
+							result.work_time + 
+							" WHERE day_id = " + 
+							result.last_day_id;
+					
+			sql += ";" + sqlUpdate;
+
+			console.log(sql);
+
+			var query = client.query(sql, function(err, result) {
+				// Done getting data from DB; disconnect the client
+				client.end(function(err) {
+					if (err) throw err;
+					getDaysAndTasks(req, res);
+				});
+			});
+		});
+		console.log("Inserted new task_day value...");		
+	}, req, res);
+}
+
+function insertNewTaskIntoDb(callback, req, res) {
+	var course = req.body.course;
+	var description = req.body.description;
+	var due = req.body.due + ":00";
+	var work_time = req.body.work_time;
+	var dayId = req.body.day_id;
+
+	console.log("Inserting new task into DB");
+	console.log(req.body);
+
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		var sql = "INSERT INTO task" 			+
+					"("							+
+					  "class"					+
+					", description"				+
+					", due_time"				+
+					", total_work_time"			+
+					") "						+
+					"VALUES"					+
+					"("							+ 
+					  "'" + course 		+ "'"	+ 
+					", '" + description + "'"	+ 
+					", '" + due 		+ "'"	+ 
+					", "  + work_time			+ 
+					") "						+
+					"RETURNING task_id";
+
+		console.log(sql);
+
+		var query = client.query(sql, function(err, result) {
+			// Done getting data from DB; disconnect the client
+			
+			client.end(function(err) {
+				if (err) throw err;
+				console.log("TEST " + result.rows[0].task_id);
+			});
+
+			callback(null, {
+				last_task_id: result.rows[0].task_id,
+				last_day_id: dayId,
+				work_time: work_time
+			});
+		});
+	});
+	console.log("Inserted new task...");
+}
+
+function updateAvailableTime(request, response) {	
+	// console.log("task id i got back yo: " + result.last_task_id);
+	// console.log("day id i got back yo: " + result.last_day_id);
+	// console.log("work time i got back yo: " + result.work_time);
+
+	console.log("Updating day table...");
+
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+		}
+
+		var sql = "UPDATE day SET total_available_time_day = " + 
+						request.body.available_time + 
+						" WHERE day_id = " + 
+						request.body.day_id;
+
+		console.log(sql);
+
+		var query = client.query(sql, function(err, result) {
+			// Done getting data from DB; disconnect the client
+			client.end(function(err) {
+				if (err) throw err;
+				getDaysAndTasks(request, response);
+			});
+		});
+	});
+	console.log("Updated available time in day table...");			
+}
+
+function deleteTask(request, response) {	
+	console.log("Deleting task #" + request.body.task_id);
+
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+		}
+
+		// UPDATE day SET total_work_time_day = total_work_time_day - 
+		// (select total_work_time from task where task_id = 2) 
+		// where day_id = (select day_id from task_day where task_id = 2);
+
+		var sqlUpdate = "UPDATE day SET total_work_time_day = total_work_time_day - " + 
+							"(SELECT total_work_time FROM task WHERE task_id = " +
+							request.body.task_id + ") " +
+						" WHERE day_id = " + 
+							"(SELECT day_id FROM task_day WHERE task_id = " + 
+							request.body.task_id + ")";
+
+		console.log(sqlUpdate);
+
+		var sql = "DELETE FROM task_day WHERE task_id = " + request.body.task_id + ";" +
+				  "DELETE FROM task     WHERE task_id = " + request.body.task_id;
+		
+		console.log(sql);
+
+		sqlUpdate += ";" + sql;
+
+		var query = client.query(sqlUpdate, function(err, result) {
+			// Done getting data from DB; disconnect the client
+			client.end(function(err) {
+				if (err) throw err;
+				getDaysAndTasks(request, response);
+			});
+		});
+	});
+	console.log("Deleted task...");			
+}
---
+++ weekly_planner_db_heroku.sql
@@ -0,0 +1,83 @@
+-- Restore to initial state
+
+DROP SCHEMA public CASCADE;
+
+CREATE SCHEMA public;
+
+GRANT ALL ON SCHEMA public TO postgres;
+
+GRANT ALL ON SCHEMA public TO public;
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+-- DROP USER IF EXISTS test_user;
+
+
+-- Create tables
+
+CREATE TABLE planner
+(
+   planner_id          SERIAL         PRIMARY KEY
+,  first_name          VARCHAR(100)   
+,  middle_initial      CHAR(1)
+,  last_name           VARCHAR(100)   
+,  email               VARCHAR(200)                 UNIQUE
+,  username            VARCHAR(100)   NOT NULL      UNIQUE
+,  password            VARCHAR(100)   NOT NULL
+,  planner_image_url   TEXT                      
+);
+
+CREATE TABLE task
+(
+   task_id           SERIAL    PRIMARY KEY
+,  class             CHAR(8)   NOT NULL
+,  description       TEXT      NOT NULL
+,  due_time          TIME      NOT NULL
+,  total_work_time   NUMERIC   NOT NULL
+);
+
+CREATE TABLE day
+(
+   day_id                     SERIAL    PRIMARY KEY
+,  name                       CHAR(9)   NOT NULL
+,  total_work_time_day        NUMERIC   NOT NULL      DEFAULT 0
+,  total_available_time_day   NUMERIC   NOT NULL      DEFAULT 0
+);
+
+CREATE TABLE task_day
+(
+   task_day_id   SERIAL   PRIMARY KEY
+,  task_id       INT      REFERENCES task(task_id)
+,  day_id        INT      REFERENCES day(day_id)
+);
+
+CREATE TABLE week
+(
+   week_id                     SERIAL    PRIMARY KEY
+,  total_work_time_week        NUMERIC   NOT NULL      DEFAULT 0
+,  total_available_time_week   NUMERIC   NOT NULL      DEFAULT 0
+);
+
+-- Add initial values
+
+INSERT INTO day (name) VALUES ('Monday');
+
+INSERT INTO day (name) VALUES ('Tuesday');
+
+INSERT INTO day (name) VALUES ('Wednesday');
+
+INSERT INTO day (name) VALUES ('Thursday');
+
+INSERT INTO day (name) VALUES ('Friday');
+
+INSERT INTO day (name) VALUES ('Saturday');
+
+INSERT INTO day (name) VALUES ('Sunday');
+
+-- Create test user
+
+-- CREATE USER test_user WITH PASSWORD 'password';
+
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON planner, day, task, task_day, week TO test_user;
+
+-- GRANT USAGE, SELECT ON SEQUENCE planner_planner_id_seq, day_day_id_seq, task_task_id_seq, task_day_task_day_id_seq, week_week_id_seq TO test_user;
